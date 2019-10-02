d25216f54dc612f2acc78277c68c8f9a719ff56d
AHartNtkn <AHartNtkn@users.noreply.github.com>
2019-10-02 09:13:39 -0700

    simplify median code

--- ToolBox/stats_tools.py
+++ ToolBox/stats_tools.py
@@ -1,34 +1,29 @@
 class Stats:
     """ A class containing util functions for stats. """
     def __init__(self, data):
-        self.data = data
-    
+        self.data = list(data)
+        self.len = len(self.data)
+        self.sum = sum(self.data)
+        self.sorted = sorted(self.data)
+
     def mean(self):
-        return sum(self.data)/len(self.data)
-    
+        return self.sum/self.len
+
     def mode(self):
         return max(self.data, key=self.data.count)
-    
-    def median(self):
-        # Finds the median of a pre-sorted list
-        def premedian(l):
-          if len(l) == 0:
-            raise ValueError('Input must be a non-empty list.')
-          elif len(l) == 1:
-            return l[0]
-          elif len(l) == 2:
-            return sum(l)/2
-          else: return premedian(l[1:-1])
 
-        return premedian(sorted(self.data))
-    
+    def median(self):
+        if self.len % 2 == 0:
+            return sum(self.sorted[self.len//2-1:self.len//2+1])/2
+        else:
+            return self.sorted[self.len//2]
+
     def var(self):
-        return sum(list(map(lambda x: (x-self.mean())**2, self.data)))/(len(self.data)-1)
-    
+        return sum(list(map(lambda x: (x-self.mean())**2, self.data)))/(self.len-1)
+
     def std(self):
         return self.var()**(1/2)
-    
+
     def coef_of_var(self):
         return self.std()/self.mean()
 
-    
