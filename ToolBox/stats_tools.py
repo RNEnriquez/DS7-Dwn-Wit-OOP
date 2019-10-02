7611e1077e3fcf592ebfb0ecca08eb3e23cc96ec
johanaluna <johanaluna@users.noreply.github.com>
2019-10-02 16:13:56 -0400

    correct file

--- ToolBox/stats_tools.py
+++ ToolBox/stats_tools.py
@@ -13,6 +13,7 @@ class Stats():
     def __init__(self, list):
         self.list = list
 
+
     def mean(self):
         self.x = reduce((lambda x, y: x + y), self.list)
         return(self.x/len(self.list))
@@ -25,7 +26,6 @@ class Stats():
         if len(self.list) % 2 == 0:
             return(sum((self.sort_num[self.middlelist - 1],
                          self.sort_num[self.middlelist]))/2.0)
-
         else:
             return(self.sort_num[self.middlelist])
 
@@ -33,20 +33,18 @@ class Stats():
     def mode(self):
         return max(set(self.list), key=self.list.count)
 
+
     def variance(self,):
-        self.x = reduce((lambda x, y: x + y), self.list)
-        self.mean_r = (self.x/len(self.list))
+        self.mean_r = self.mean()
         return(sum((x - self.mean_r)**2 for x in self.list)/len(self.list))
 
-    def standard_deviation(self):
-        self.x = reduce((lambda x, y: x + y), self.list)
-        self.mean_r = (self.x/len(self.list))
-        self.variance = (sum((x - self.mean_r)**2 for x in self.list)/len(self.list))
-        return np.sqrt(self.variance)
 
-    def coefficient_variation(self, stand_dev, result_mean):
-        self.x = reduce((lambda x, y: x + y), self.list)
-        self.mean_r = (self.x/len(self.list))
-        self.variance = (sum((x - self.mean_r)**2 for x in self.list)/len(self.list))
-        self.standard_deviation = np.sqrt(self.variance)
-        return ((self.standard_deviation/self.mean)*100)
+    def standard_deviation(self):
+        self.variance_r = self.variance()
+        return np.sqrt(self.variance_r)
+
+
+    def coefficient_variation(self):
+        self.std_r = self.standard_deviation()
+        self.mean_r = self.mean()
+        return ((self.std_r/self.mean_r)*100)
